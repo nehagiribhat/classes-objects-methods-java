5595915ae58179e58ad540cf4851f24db983ea29
nehagiribhat <nehagiribhat@gmail.com>
2020-07-18 00:18:32 +0530

    ADDED: Concepts od Classes, Objects and Methods

---
+++ MyClass.java
@@ -0,0 +1,75 @@
+class First {
+	public void display() {
+		System.out.println("One");
+	}
+}
+
+//inheritance
+class Second extends First {
+	
+	@Override
+	public void display() {
+		System.out.println("Second");
+	}
+
+	public int add(int x, int y) {
+		return x+y;
+	}
+
+	//Overload
+	public double add(double x,double y) {
+		return x+y;
+	}
+}
+
+
+//encapsulation example
+class EncapsulTest {
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String newName) {
+		name = newName;
+	}
+}
+
+//abstraction
+abstract class TwoWheeler {
+	
+	public abstract void run();
+}
+
+
+class Honda extends TwoWheeler{
+
+	public void run(){
+		System.out.println("\nbike is Running..");
+	}
+}
+
+
+class MyClass {
+	
+	public static void main(String[] args) {
+
+		First a=new First();
+		a.display();
+
+		Second b=new Second();
+		b.display();
+
+		System.out.println(b.add(4,2));
+		System.out.println(b.add(5.,2.));	 //polymorphism
+
+		EncapsulTest encap = new EncapsulTest();
+		encap.setName("Sandeep's");
+		System.out.print("Name : " + encap.getName() );
+
+		TwoWheeler test = new Honda();
+		test.run();
+	}
+}
